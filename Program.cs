64271e2f6f8bb15552ef4a635d934a9edf15c763
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 14:35:01 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LearningExponentMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(4, 3));
+            Console.ReadLine();
+
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+            return result;
+        }
+    }
+}
